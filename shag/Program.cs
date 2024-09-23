9c0d938fb0091f467cbbf0e8e494fb36a64b5f85
matvi <matvi@DESKTOP-GE60QO8>
2024-09-23 22:39:28 +0300

    Add project files.

---
+++ shag/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter age to find out if you can go to study in shag:");
+int age = int.Parse(Console.ReadLine());
+if (age >= 15 && age <= 55)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("you fit SIGMA MEN");
+    Console.ResetColor();
+}
+
+
+else
+{
+    Console.ForegroundColor= ConsoleColor.Red;
+    Console.WriteLine("You not fit Skuf!!!!");
+    Console.ResetColor();
+}
